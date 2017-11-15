e43e9d8b7d7d0aa5c88f131124568827589bddb7
Luis Carranza <lukanew@Luiss-MacBook-Pro.local>
2017-11-14 23:30:25 -0500

    Module 48

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
 import { CoursesService } from './courses.service';
+import { SummaryPipe } from './summary.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseComponent
+    CourseComponent,
+    SummaryPipe
   ],
+
   imports: [
-    BrowserModule
+
+    BrowserModule,
+    FormsModule
   ],
   providers: [CoursesService],
   bootstrap: [AppComponent]
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -11,13 +11,35 @@ import { CoursesService } from '../courses.service';
 export class CourseComponent  {
 
     title = "List of courses....";
+    noCourses = 30124;
+    rating = 4.9745;
+    price = 155.65;
+    releaseDate= new Date(2016,3,1);
     imageUrl="https://s3.amazonaws.com/9lessonspics/nodeapi/nodebanner.png";
+    text = "This is a new application that was created with angular and other frameworks.!";
     colspan=2;
     courses;
+    isActive=true;
 
     constructor(service:CoursesService){
        //let service = new CoursesService();
        this.courses = service.getCourses();
     }
 
+    email;
+
+    onSave($event){
+      $event.stopPropagation();
+      
+      alert('Hello world');
+      console.log($event);
+    }
+
+    onOther($event){
+      console.log("First button");
+    }
+
+    onKeyup(value){
+      alert("You pressed enter..!" + this.email);
+    }
 }
